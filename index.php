2563259cd32b93334c42a66af048025bf51a4cfe
Claudia Belforte <claubelforte@gmail.com>
2021-01-06 22:45:40 -0300

    listooo

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="description" content="Claudia Belforte">
+    <meta name="keywords" content="estudio,contable,contador,contadora,contabilidad">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        Belforte Claudia V
+    </title>
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!--Estilos-->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500&display=swap" rel="stylesheet">
+
+    <!--Scripts-->
+    <script src="https://kit.fontawesome.com/bf91ce9ef4.js" crossorigin="anonymous"></script>
+    <script src="app.js"></script>
+
+
+
+</head>
+
+<body>
+    <header id="header">
+        <!--Menu de navegación-->
+        <nav class="menu">
+            <div class="logo-box">
+                <a href="#"><img src="img/logoeste.png" class="logo-nav" alt=""></a>
+                <span class="btn-menu"><i class="fas fa-bars"></i></span>
+            </div>
+            <div class="list-container">
+                <ul class="lists">
+                <li><a href="#header">Portfolio</a></li>
+                <li><a href="#acerca">Acerca de mí</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#micv">Mi CV</a></li>
+                <li><a href="#footer">Contacto</a></li>
+            </ul>
+            </div>
+        </nav>
+    </header>
+        <div class="img-header">
+            <div class="inicio">
+                <p>BELFORTE CLAUDIA VALERIA</p>
+                <hr>
+                <h1>FRONT-END DEVELOPER</h1>
+                <button id="abajo">Ver más</button>
+            </div>
+        </div>
+        <div class="skew-abajo"></div>
+       
+        <section id="micv" class="micv">
+           
+            <div> <h2>Mi CV</h2>
+            <hr>
+            <div class="cv">
+            <div class="boton">
+                <p>Podés descargar mi CV y ver tanto mis datos, como mi formación académica, mucho más detallada.
+                </p>  <a class="botoncvv" target="_blank" href="documentos/ClaudiaBelforteCv.pdf"> Descarga mi CV</a>
+            </div>
+            <img src="img/claucv.gif" alt="">
+        </div>
+        </div>
+        
+        </section>
+        <div class="container-tecnologias">
+                
+                
+            <br>
+              <h4>TECNOLOGÍAS QUE MANEJO</h4>
+              
+              <br>
+              <div class="banner">
+                  
+                  <div class="banner-item"> <p class="banner-txt">HTML5 <br><i style="color: #e62646;" class="fab fa-html5"></i></p> </div>
+                  <div class="banner-item"> <p class="banner-txt">CSS3 <br><i style="color: rgb(39, 110, 218);" id="css" class="fab fa-css3-alt"></i></p> </div>
+                  <div class="banner-item"> <p class="banner-txt">JAVASCRIPT <br><i style="color: rgb(238, 238, 58);" class="fab fa-js-square"></i></p> </div>
+                  <div class="banner-item"> <p class="banner-txt">BOOTSTRAP <br><i style="color: purple;" class="fab fa-bootstrap"></i></p></div>
+                  <div class="banner-item"> <p class="banner-txt">GIT <br> <i style="color: rgb(206, 77, 37);" class="fab fa-git-alt"></i></p> </div>
+              </div>
+           </div>
+    
+    
+    <section id="proyectos" class="proyectos">
+        <div class="skew-arriba"></div>
+        <div class="deg-background">
+            
+        </div>
+        <div class="ejeZproject">
+            <div class="container-project">
+                <div class="project-title">
+                    <h2>Proyectos</h2>
+                    <hr>
+                </div>
+                <div class="project-img">
+                    <img src="img/rompiendocodigos.png" alt="">
+                    <img src="img/trueley.png" alt="">
+                    <img src="img/navarro-insaurralde.png" alt="">
+                    <img src="img/diariodigital.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="skew-abajo"></div>
+    </section>
+    <section id="acerca" class="acerca">
+        <div class="titulo-acerca">
+         <h2>Acerca de mí</h2>
+         <hr>
+        </div>
+    
+         <div class="box-acerca">
+          <div class="card-acerca">
+            <div class="card-img"><img src="img/claupc.gif" alt="" ></div>
+            <div class="acerca-text">
+             <p>¡Hola! Gracias por visitar mi portfolio.
+                Mi nombre es <b>Claudia</b>, tengo 25 años, vivo de CABA, Argentina y actualmente soy estudiante autodidacta de desarrollo Front-end. 
+                Quiero desarrollar mi carrera en IT porque es lo que más me apasiona ya que me considero muy entusiasta y creativa, y siento que esta carrera permite dejar fluir todas mis cualidades y me permite esplotar al 100% mi creatividad. 
+                
+              </p>
+              <p>El desarrollo web es una parte fundamental de mi día a día y lo que más quisiera hacer es poder vivir de ello.</p>
+              
+            </div>
+            
+             <div class="skew-arriba"></div>
+          </div>
+          
+         </div>
+         <div class="skew-abajo"></div>
+    </section>
+    
+    <footer id="footer" class="footer">
+        <div class="skew-arriba"></div>
+        <div class="deg-footer"></div>
+        <div class="ejeZfooter">
+            <div class="footer-content">
+                <div class="footer-title">
+                    <h2>Contacto</h2>
+                    <hr>
+                </div>
+                <div class="formulario-content">
+                    <form action="index.php" id="formulario">
+                        <input type="text" id="user" name="nombre" placeholder="Nombre">
+                        <input type="email" id="email" name="email" placeholder="Email">
+                        <textarea name="mensaje" id="mensaje" name="mensaje" cols="30" placeholder="Envíame un mensaje"
+                            rows="10"></textarea>
+                        <div class="send"><button>Enviar</button></div>
+                       
+                       <?php
+
+                    if($_SERVER["REQUEST_METHOD"] == "POST")
+                    {
+                        $nombre = $_POST["nombre"] ;
+                        $email = $_POST["email"] ;
+                        $mensaje = $_POST["mensaje"] ;
+                        if(isset($nombre))
+                         {
+                            if(isset($email))
+                            {
+                                if(isset($mensaje)) {
+                                    $para = "claubelforte@gmail.com";
+                                    $asunto = "portfolio";
+                                    $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                    $adicional = "From : claubelforte@outlook.com";
+
+                                    mail($para,$asunto,$cuerpo,$adicional);
+                                    ?>
+                                    <p>Envío exitoso</p>
+                                        <?php
+                                }
+                            }
+                        }
+                    }
+                     ?>
+                    </form>
+                </div>
+                <div class="footer-text">
+                   <p>&copy;Derechos reservados 2020 |  Hecho con <i class="far fa-heart"></i> por Claudia Belforte.  </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+    crossorigin="anonymous"></script>
+    </body>
+
+</html>
